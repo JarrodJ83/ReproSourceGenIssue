4f5d9f95eaf60b958ecf327cb87b6dd11c17a2d5
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2021-04-06 22:03:11 -0400

    Add project files.

---
+++ ReproSourceGenIssue/OpenApiSourceGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Diagnostics;
+
+namespace ReproSourceGenIssue
+{
+    [Generator]
+    public class OpenApiSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            Debugger.Launch();
+
+            // Uncommenting the next line will cause this method to never be invoked (Debugger.Launch() will not be called)
+            //var openApiReader = new Microsoft.OpenApi.Readers.OpenApiStringReader();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            //Debugger.Launch();
+        }
+    }
+}
